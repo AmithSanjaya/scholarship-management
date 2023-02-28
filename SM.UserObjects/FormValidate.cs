245c2a5dfa84da64ece710f6f4787f6c326bb698
erandas <erandasenanayake@gmail.com>
2023-02-28 21:37:02 +0530

    Student Validation

---
+++ SM.UserObjects/FormValidate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.UserObjects
+{
+    public class FormValidate
+    {
+        public int MenuID { get; set; }
+        public string InputID { get; set; }
+        public string FieldName { get; set; }
+        public string FieldValue { get; set; }
+    }
+}
